881db78ced9cb3a9ca6f133df53cb05d8deb70f6
Ray Miller <ray@1729.org.uk>
2014-05-26 08:49:02 +0100

    Update Clojure versions; update Blueprints to 2.5.0

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [potemkin "0.2.0"]                 
-                 [com.tinkerpop.blueprints/blueprints-core "2.4.0"]]
+                 [com.tinkerpop.blueprints/blueprints-core "2.5.0"]]
   :profiles {:1.4    {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.6    {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
-             :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}}
+             :1.6    {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}}
   :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
